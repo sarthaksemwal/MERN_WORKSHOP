d3a721823328570fc66fe000d2d085a19be04a78
sarthak <sarthaksemwal69@gmail.com>
2024-04-05 10:55:22 +0530

    created some new files

---
+++ JavaScript/arrayfun.js
@@ -0,0 +1,6 @@
+//using the function to check wheather the given var is an array 
+const arr= [1,2,3,34]
+
+const noarr="hello"
+console.log(Array.isArray(arr))
+console.log(Array.isArray(noarr))
---
+++ JavaScript/index.js
@@ -0,0 +1,10 @@
+
+const obj={
+    name:"Sarthak",
+    "lastname":"Semwal",
+}
+
+obj.name="Cheetah";
+console.log(obj)
+
+//we can use th Json.stringfy methoud  to convert the object to the json file 
